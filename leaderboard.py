c2c39d09344d6aa47669d40b65badbc3ab5f0d30
EKO HENDRATNO <eko.hendratno@gmail.com>
2025-08-09 09:20:20 +0700

    add v1.2
    penambahan ctrl + x = untuk keluar
    penambahan ctrl + e = untuk edit url
    penambahan ctrl + r = untuk reload

--- leaderboard.py
+++ leaderboard.py
@@ -1,6 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QShortcut, QInputDialog
 from PyQt5.QtCore import QUrl, Qt
+from PyQt5.QtGui import QKeySequence
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 
 
@@ -12,8 +13,9 @@ class MainWindow(QMainWindow):
         self.browser = QWebEngineView()
         self.setCentralWidget(self.browser)
 
-        # Load halaman leaderboard
-        self.browser.load(QUrl("http://127.0.0.1:9000/presensi-edu/guest/leaderboard_usb_rfid/dcZrym"))
+        # URL default
+        self.default_url = "http://127.0.0.1:9000/presensi-edu/guest/leaderboard_usb_rfid/dcZrym"
+        self.browser.load(QUrl(self.default_url))
 
         # Enable fitur JavaScript dan penyimpanan lokal
         self.browser.settings().setAttribute(self.browser.settings().JavascriptEnabled, True)
@@ -24,13 +26,25 @@ class MainWindow(QMainWindow):
         self.browser.setFocusPolicy(Qt.StrongFocus)
         self.browser.setFocus()
 
+        # Shortcut Ctrl+X untuk keluar
+        QShortcut(QKeySequence("Ctrl+X"), self, activated=self.close)
+
+        # Shortcut Ctrl+E untuk edit URL
+        QShortcut(QKeySequence("Ctrl+E"), self, activated=self.edit_url)
+
+        # Shortcut Ctrl+R untuk reload halaman
+        QShortcut(QKeySequence("Ctrl+R"), self, activated=self.browser.reload)
+
         # Full screen
         self.showFullScreen()
+
+    def edit_url(self):
+        new_url, ok = QInputDialog.getText(self, "Edit URL", "Masukkan URL baru:", text=self.browser.url().toString())
+        if ok and new_url.strip():
+            self.browser.load(QUrl(new_url.strip()))
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MainWindow()
     sys.exit(app.exec_())
-
-
